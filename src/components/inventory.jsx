114c75e0f6f06175bdaf49b3e5fa2eb4f432a991
Lukasz Mielczarek <lukasz.mielczarek.dev@gmail.com>
2021-09-08 22:21:41 +0200

    RI-23 inventory component imprivements + added dynamic rendering

--- src/components/inventory.jsx
+++ src/components/inventory.jsx
@@ -4,10 +4,28 @@ import UserInventory from "../contexts/inventoryContext";
 
 const Inventory = (props) => {
   const inventory = useContext(UserInventory);
+
+  const renderInventory = () => {
+    const items = [];
+    for (let [item, amount] of Object.entries(inventory.inventory)) {
+      items.push([item, amount]);
+    }
+    return items.map((element) => (
+      <li>
+        {element[0]} - amount: {element[1]}{" "}
+      </li>
+    ));
+  };
+
+  const back = () => {
+    props.history.goBack();
+  };
+
   return (
     <div id="inventory">
       <h3>Inventory</h3>
-      <p>{inventory.credits}</p>
+      <p>{renderInventory()}</p>
+      <button onClick={back}>X</button>
     </div>
   );
 };
