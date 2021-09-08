910975b74e39e6b771b206a1bd45546b9072baab
Lukasz Mielczarek <lukasz.mielczarek.dev@gmail.com>
2021-09-08 21:24:30 +0200

    RI-15 contexts (shop, inventory and user improved in app.js file)

--- src/App.js
+++ src/App.js
@@ -7,57 +7,98 @@ import Footer from "./components/footer";
 // import Controller from "./components/controller";
 import UserContext from "./contexts/userContext";
 import "./App.css";
-// import InventoryContext from "./contexts/inventoryContext";
+import InventoryContext from "./contexts/inventoryContext";
+import ShopContext from "./contexts/shopContext";
 
 export default function App() {
   const [userInfo, setUserInfo] = useState({
-    name: "Unnamed",
+    name: "Guest",
     lvl: 1,
+    rocketLvl: 1,
     exp: 0,
     currentPlanet: "Earth",
+  });
+
+  const [userInventory, setUserInventory] = useState({
+    credits: 500,
+    word: 0,
+    stardust: 0,
+    steel: 0,
+    aluminum: 0,
+  });
+
+  const [shop] = useState({
+    word: 450,
+    stardust: 5000,
+    steel: 3500,
+    aluminum: 4000,
   });
 
   useEffect(() => {
     handleCalcLvl();
   }, [userInfo.exp]);
 
-  // WiP...
-  // const [userInventory, setUserInventory] = useState({
-  //   credits: 500,
-  // });
+  // credits
+  const handleAddCredits = (amount) => {
+    const userInventoryDummy = { ...userInventory };
+    userInventoryDummy.credits = userInventory.credits + amount;
+    setUserInventory(userInventoryDummy);
+  };
 
-  // const handleAddCredits = (amount) => {
-  //   const userInventoryDummy = { ...userInventory };
-  //   userInventoryDummy.credits = userInventory.credits + amount;
-  //   setUserInventory(userInventoryDummy);
-  // };
+  const handleSubtractCredits = (amount) => {
+    const userInventoryDummy = { ...userInventory };
+    userInventoryDummy.credits = userInventory.credits - amount;
+    setUserInventory(userInventoryDummy);
+  };
 
-  // const handleSubtractCredits = (amount) => {
-  //   const userInventoryDummy = { ...userInventory };
-  //   userInventoryDummy.credits = userInventory.credits - amount;
-  //   setUserInventory(userInventoryDummy);
-  // };
+  // subtract item
+  const handleSubtractItem = (item, amount) => {
+    const userInventoryDummy = { ...userInventory };
+    userInventoryDummy[item] = userInventory[item] - amount;
+    setUserInventory(userInventoryDummy);
+  };
 
-  //
+  // free item
+  const handleAddItem = (item, amount) => {
+    const userInventoryDummy = { ...userInventory };
+    userInventoryDummy[item] = userInventory[item] + amount;
+    setUserInventory(userInventoryDummy);
+  };
 
+  // shop
+  const handleBuyItem = (item, amount, price, multiplier) => {
+    if (userInventory.credits >= price * multiplier) {
+      const userInventoryDummy = { ...userInventory };
+      userInventoryDummy[item] = userInventory[item] + amount;
+      userInventoryDummy.credits = userInventory.credits - price * multiplier;
+      setUserInventory(userInventoryDummy);
+    } else {
+      alert("Not enaugh credits!");
+    }
+  };
+
+  // lvl
   const handleCalcLvl = () => {
     const userDataDummy = { ...userInfo };
-    userDataDummy.lvl = userInfo.exp / 100;
+    userDataDummy.lvl = userInfo.exp / 1000;
     setUserInfo(userDataDummy);
   };
 
+  // exp
   const handleAddExp = (amount) => {
     const userDataDummy = { ...userInfo };
     userDataDummy.exp = userDataDummy.exp + amount;
     setUserInfo(userDataDummy);
   };
 
+  // username
   const handleSetName = (name) => {
     const userDataDummy = { ...userInfo };
     userDataDummy.name = name;
     setUserInfo(userDataDummy);
   };
 
+  // current planet
   const handleSetPlanet = (place) => {
     const userDataDummy = { ...userInfo };
     userDataDummy.currentPlanet = place;
@@ -65,21 +106,33 @@ export default function App() {
   };
 
   return (
-    // <InventoryContext.Provider value={userInventory}>
-    <UserContext.Provider
-      value={{
-        user: userInfo,
-        onAddExp: handleAddExp,
-        onSetPlanet: handleSetPlanet,
-        onSetName: handleSetName,
-      }}
+    <ShopContext.Provider
+      value={{ shopInventory: shop, buyItem: handleBuyItem }}
     >
-      <Switch>
-        <Route path="/galaxy" component={Galaxy} />
-        <Route path="/" exact component={Welcome} />
-      </Switch>
-      <Footer />
-    </UserContext.Provider>
-    // </InventoryContext.Provider>
+      <InventoryContext.Provider
+        value={{
+          inventory: userInventory,
+          addItem: handleAddItem,
+          subtractItem: handleSubtractItem,
+          addCredits: handleAddCredits,
+          subtractCredits: handleSubtractCredits,
+        }}
+      >
+        <UserContext.Provider
+          value={{
+            user: userInfo,
+            onAddExp: handleAddExp,
+            onSetPlanet: handleSetPlanet,
+            onSetName: handleSetName,
+          }}
+        >
+          <Switch>
+            <Route path="/galaxy" component={Galaxy} />
+            <Route path="/" exact component={Welcome} />
+          </Switch>
+          <Footer />
+        </UserContext.Provider>
+      </InventoryContext.Provider>
+    </ShopContext.Provider>
   );
 }
--- src/contexts/inventoryContext.js
+++ src/contexts/inventoryContext.js
@@ -1,5 +1,6 @@
 import React from "react";
 
 const InventoryContext = React.createContext();
+InventoryContext.displayName = "InventoryContext";
 
 export default InventoryContext;
