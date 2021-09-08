50a6e4a76194670f0cc325a4262dd99cc43e5963
Lukasz Mielczarek <lukasz.mielczarek.dev@gmail.com>
2021-09-08 21:46:50 +0200

    RI-18 shop

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserInventory from "../contexts/inventoryContext";
+import ShopInventory from "../contexts/shopContext";
 import "./shop.css";
 
 const Shop = (props) => {
+  const inventory = useContext(UserInventory);
+  const shop = useContext(ShopInventory);
+
+  const shopInv = () => {
+    const items = [];
+    for (let [item, price] of Object.entries(shop.shopInventory)) {
+      items.push([item, price]);
+    }
+    return items.map((element) => (
+      <li>
+        {element[0]} - {element[1]}[!]{" "}
+        <button
+          onClick={() => {
+            shop.buyItem(element[0], 1, element[1], 1);
+          }}
+        >
+          Buy
+        </button>
+      </li>
+    ));
+  };
+
+  const back = () => {
+    props.history.goBack();
+  };
+
   return (
     <div id="shop">
       <h3>Shop</h3>
+      <p>Available credits: {inventory.inventory.credits}</p>
+      <div>{shopInv()}</div>
+      <button onClick={back}>X</button>
     </div>
   );
 };
