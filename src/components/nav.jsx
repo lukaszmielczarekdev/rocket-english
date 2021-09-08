78a02358982b0eb3b71412ee55c43202737a0566
Lukasz Mielczarek <lukasz.mielczarek.dev@gmail.com>
2021-09-08 22:17:10 +0200

    RI-21 navbar links to the components

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,31 +1,42 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "../contexts/userContext";
+import InventoryContext from "../contexts/inventoryContext";
 import "./nav.css";
 
 const Nav = (props) => {
   const user = useContext(UserContext);
+  const inventory = useContext(InventoryContext);
 
   return (
     <nav id="nav" className="text-center split container">
       <Link to="/">
         <h1>ROCKET ENGLISH</h1>
       </Link>
-      {/* Test buttons */}
-      {/* <button onClick={() => user.onSetName("Luke")}>Set Name</button>
-      <button onClick={() => user.onAddExp(100)}>Add exp</button>
-      <button onClick={() => user.onSetPlanet("Tatooine")}>Set planet</button> */}
       <ul>
+        <li>
+          <Link to="/galaxy/earth">Quiz - earth</Link>
+        </li>
+        <li>
+          <Link to="/galaxy/mine">Mine</Link>
+        </li>
+        <li>
+          <Link to="/galaxy/shop">Shop</Link>
+        </li>
         <li>
           <Link to="/galaxy/inventory">Inventory</Link>
         </li>
         <li>
-          <Link to="/galaxy/trophies">Trophies</Link>
+          <Link to="/galaxy/casino">Casino</Link>
+        </li>
+        <li>
+          <Link to="/galaxy/factory">Factory</Link>
         </li>
         <li>Planet: {user.user.currentPlanet}</li>
         <li>Lvl: {user.user.lvl}</li>
         <li>Exp: {user.user.exp}</li>
         <li>Name: {user.user.name}</li>
+        <li>Credits: {inventory.inventory.credits}</li>
       </ul>
     </nav>
   );
