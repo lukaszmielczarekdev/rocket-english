a1e1cfde33baed4f46a3a8c15b0ab751ac1b466e
Lukasz Mielczarek <lukasz.mielczarek.dev@gmail.com>
2021-09-26 12:24:47 +0200

    RI-60 favicon

--- src/utils/renders.js
+++ src/utils/renders.js
@@ -2,8 +2,8 @@ const renders = {
   logo: (webpImg, pngImg, src, alt) => {
     return (
       <picture>
-        <source srcset={webpImg} type="image/webp" />
-        <source srcset={pngImg} type="image/png" />
+        <source srcSet={webpImg} type="image/webp" />
+        <source srcSet={pngImg} type="image/png" />
         <img src={src} type="image/png" width="100em" height="auto" alt={alt} />
       </picture>
     );
