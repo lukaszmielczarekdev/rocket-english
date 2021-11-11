3d3a523cdd2fca9eac50e831c26c142f06526693
Lukasz Mielczarek <lukasz.mielczarek.dev@gmail.com>
2021-11-11 21:11:34 +0100

    RI-106 ability to delete favorite words (favorites component)

--- src/components/universal/articleCard.jsx
+++ src/components/universal/articleCard.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import InventoryContext from "../../contexts/inventoryContext";
 
 const PlaceBasic = (props) => {
+  const inventory = useContext(InventoryContext);
   return (
     <article className="padding-places border carousel-card">
-      <h4>{props.title}</h4>
+      <div className="fav-title">
+        <h4>{props.title}</h4>
+        <i
+          onClick={() => inventory.removeFromFavorite(props.title)}
+          class="fas fa-times fav-del-button"
+        ></i>
+      </div>
       <p className="align-center">{props.description}</p>
-      {/* <button className="button small">Remove</button> */}
     </article>
   );
 };
