019f7357eb6556c6b3ff32d915abd81082238cc2
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2021-11-21 17:29:42 +0100

    RI-122 menu component layout improvement, more features included

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -9,7 +9,11 @@ import { Redirect } from "react-router-dom";
 import Modal from "react-modal";
 import { useForm } from "react-hook-form";
 import Nav from "./nav";
+import Footer from "./footer";
 import "./welcome.css";
+import thumbnail_rocket from "../images/thumbnail-rocket.jpg";
+import thumbnail_learn from "../images/thumbnail-learn.jpg";
+import thumbnail_light from "../images/thumbnail-light.jpg";
 
 const Welcome = (props) => {
   const user = useContext(UserContext);
@@ -35,6 +39,12 @@ const Welcome = (props) => {
     }
   };
 
+  const ifCurrent = (planet) => {
+    return general.general.availablePlanets[planet].available
+      ? "current-planet"
+      : "";
+  };
+
   const handleSubmitUserData = (data) => {
     user.onSetName(data);
     general.setAvailablePlanet("crion");
@@ -46,7 +56,7 @@ const Welcome = (props) => {
   const renderResetProgress = () => {
     if (general.general.availablePlanets["crion"].discovered) {
       return (
-        <button onClick={toggleModal} className="button small button-margin">
+        <button onClick={toggleModal} className="button large button-margin">
           New game
         </button>
       );
@@ -134,15 +144,13 @@ const Welcome = (props) => {
   const renderContinueGame = () => {
     if (general.general.availablePlanets["crion"].discovered) {
       return (
-        <button className="button small button-margin">
-          <Link
-            onClick={makeCurrentPlanetAvailableAgain()}
-            to={`/${user.user.currentPlanet}`}
-            style={{ textDecoration: "none" }}
-          >
-            Continue
-          </Link>
-        </button>
+        <Link
+          onClick={makeCurrentPlanetAvailableAgain()}
+          to={`/${user.user.currentPlanet}`}
+          style={{ textDecoration: "none" }}
+        >
+          <button className="button large button-margin">Continue</button>
+        </Link>
       );
     }
   };
@@ -162,32 +170,120 @@ const Welcome = (props) => {
   };
 
   return (
-    <div id="welcome" className="welcome-wrapper">
+    <div className="welcome-wrapper flex-auto">
       <div className="menu-navbar">
         <Nav />
       </div>
       {renderOrRedirect("menu")}
-      <section className="planet-container padding">
+      <section id="welcome" className="welcome-container padding">
         <div>
           <article className="padding-places">
-            <p>
-              Learn English by exploring the solar system and taking part in
-              missions.
-              <br />
-              Travel between planets learning interesting facts about them.
-              <br />
-              Visit numerous interesting places on planets, gain experience and
-              materials.
-              <br />
-              Upgrade your rocket to discover new planets and surprises.
-            </p>
+            <div id="intro">
+              <header className="intro-split header-container">
+                <article className="content">
+                  <h2>explore</h2>
+                  <hr className="underline" />
+                  <p className="planet-description">
+                    Learn English by exploring the solar system and taking part
+                    in missions. Travel between planets learning interesting
+                    facts about them. Visit numerous interesting places on
+                    planets, gain experience and materials. Upgrade your rocket
+                    to discover new planets and surprises.
+                  </p>
+                </article>
+                <article className="content menu-title">
+                  <div className="places-header">
+                    <h2>the galaxy</h2>
+                    <hr className="underline-places" />
+                  </div>
+                  <nav className="menu-nav">
+                    {general.general.newGame && renderLogin()}
+                    {!general.general.newGame && renderResetProgress()}
+                    {!general.general.newGame && renderContinueGame()}
+                  </nav>
+                  <a href="#gallery">
+                    <i class="nav-arrow far fa-arrow-alt-circle-down fa-2x"></i>
+                  </a>
+                </article>
+              </header>
+            </div>
+            <section id="gallery" className="planet-split padding-1rem">
+              <article className="content menu-title">
+                <div className="places-header">
+                  <h2>enjoy</h2>
+                  <hr className="underline-places" />
+                </div>
+              </article>
+              <article></article>
+            </section>
 
-            <nav>
-              {general.general.newGame && renderLogin()}
-              {!general.general.newGame && renderResetProgress()}
-              {!general.general.newGame && renderContinueGame()}
-            </nav>
-
+            <section className="planet-info-container planet-split">
+              <article className="flex-centered">
+                <h4>play</h4>
+                <p className="thumbnail logo-place image fit margin-bottom-0">
+                  <img
+                    src={thumbnail_light}
+                    alt="thumbnail"
+                    width="100em"
+                    height="auto"
+                  />
+                </p>
+                <p className="width-80">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                  a animi inventore voluptatum natus. Rem vel fugit illo dolores
+                  praesentium.
+                </p>
+              </article>
+              <article className="flex-centered">
+                <h4>learn</h4>
+                <p className="thumbnail logo-place image fit margin-bottom-0">
+                  <img
+                    src={thumbnail_learn}
+                    alt="thumbnail"
+                    width="100em"
+                    height="auto"
+                  />
+                </p>
+                <p className="width-80">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                  a animi inventore voluptatum natus. Rem vel fugit illo dolores
+                  praesentium.
+                </p>
+              </article>
+              <article className="flex-centered">
+                <h4>explore</h4>
+                <p className="thumbnail logo-place image fit margin-bottom-0">
+                  <img
+                    src={thumbnail_rocket}
+                    alt="thumbnail"
+                    width="100em"
+                    height="auto"
+                  />
+                </p>
+                <p className="width-80">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                  a animi inventore voluptatum natus. Rem vel fugit illo dolores
+                  praesentium.
+                </p>
+              </article>
+            </section>
+            <section>
+              <ul className="sidebar">
+                <li>Location:</li>
+                <li className={ifCurrent("crion")}>crion</li>
+                <li className={ifCurrent("therion")}>therion</li>
+                <li className={ifCurrent("crystalia")}>crystalia</li>
+                <li className={ifCurrent("thalia")}>thalia</li>
+                <li className={ifCurrent("bathea")}>bathea</li>
+                <li className={ifCurrent("axios")}>axios</li>
+                <li className={ifCurrent("desertia")}>desertia</li>
+                <li className={ifCurrent("xillon")}>xillon</li>
+                <li className={ifCurrent("centuria")}>centuria</li>
+              </ul>
+            </section>
+            <a href="#top">
+              <i class="rotate180deg nav-arrow far fa-arrow-alt-circle-down fa-2x"></i>
+            </a>
             <Modal
               id="modal"
               style={modalStyle}
@@ -213,6 +309,7 @@ const Welcome = (props) => {
           </article>
         </div>
       </section>
+      <Footer />
     </div>
   );
 };
